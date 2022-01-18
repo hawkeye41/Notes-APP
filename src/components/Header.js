7c275ad7892819f950a3b248290a083b032b848f
saurabh meena <57886471+hawkeye41@users.noreply.github.com>
2022-01-18 09:03:06 +0530

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+
+// import {FaToggleOn} from "react-icons/fa"
+
 import './Note.css';
+
 const Header=({handleDarkMode})=>{
+  
     return(
         <div className="header">
             <h1>Notes</h1>
-            <button onClick={()=> handleDarkMode((preDarkMode)=>!preDarkMode)} className="save" id="toggle">Dark</button>
+            <button onClick={()=> handleDarkMode((preDarkMode)=>!preDarkMode)} id="toggle"className="save">
+                 <span id="toggle-circle"></span>
+                </button>
         </div>
     )
 }
